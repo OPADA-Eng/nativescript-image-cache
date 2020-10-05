b512e198cb35dd403b19b27d18bfad83cf028d34
Mohamad Opada Al-Bosh <opada-eng@Mohamads-MacBook-Pro.local>
2020-10-05 13:40:13 +0300

    export functions

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,6 +1,49 @@
-import { Common } from './image-cache.common';
-export declare class ImageCache extends Common {
-  // define your typings manually
-  // or..
-  // take the ios or android .d.ts files and copy/paste them here
+import { View } from 'tns-core-modules/ui/core/view';
+
+export class WebImage extends View {
+  /**
+   * Image src, either res://, http://, ~/... or absolute path
+   */
+  src: string;
+
+  /**
+   * Is the image loading or not?
+   */
+  isLoading: boolean;
+
+  /**
+   * aspectFit, aspectFill, fill or none
+   */
+  stretch: string;
+
+  /**
+   * Native android object
+   */
+  android: any /* com.facebook.drawee.view.SimpleDraweeView */;
+
+  /**
+   * Native ios object
+   */
+  ios: any /* UIImageView */;
 }
+
+/**
+ * Clear cached images
+ */
+export function clearCache(): void;
+
+/**
+ * For android only: initialize to use cache
+ */
+export function initialize(): void;
+
+/**
+ * To initialize the plugin on angular for both android and iOS
+ */
+export function initializeOnAngular(): void;
+
+/**
+ * To set the number of days in which to expire cache
+ * @param days : limit of the cache
+ */
+export function setCacheLimit(days: number): void;
